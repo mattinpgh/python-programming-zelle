6a3d135aabe507f2024df805570b8cf6db74073d
Matt Smith <matthew.a.w.smith@gmail.com>
2025-03-08 14:27:18 -0500

    Configure linter. Style triangle_info

--- 06_chapter/triangle_info.py
+++ 06_chapter/triangle_info.py
@@ -1,40 +1,44 @@
-from graphics import *
 from math import sqrt
+from graphics import GraphWin, Text, Polygon, Point
+
 
 def get_length(point1, point2):
     dx = point1.getX() - point2.getX()
     dy = point1.getY() - point2.getY()
-    return sqrt((dx ** 2) + (dy ** 2)) 
+    return sqrt((dx ** 2) + (dy ** 2))
+
 
 def get_perimeter(point1, point2, point3):
     lst = [
-        get_length(point1, point2), 
+        get_length(point1, point2),
         get_length(point2, point3),
         get_length(point3, point1)
     ]
     return sum(lst)
 
+
 def get_area(point1, point2, point3):
-    a = get_length(point1, point2) 
+    a = get_length(point1, point2)
     b = get_length(point2, point3)
     c = get_length(point3, point1)
     s = (a+b+c)/2
     return sqrt(s*(s-a)*(s-b)*(s-c))
 
+
 def main():
     win = GraphWin("Triangle Information", 1280, 800)
     win.setCoords(-10, -10, 10, 10)
 
-    message_display = Text(Point(0, -8), "Click in three locations to create a triangle.")
+    message = "Click in three locations to create a triangle."
+    message_display = Text(Point(0, -8), message)
     message_display.draw(win)
 
     p1 = win.getMouse()
     p2 = win.getMouse()
     p3 = win.getMouse()
-    triangle = Polygon(p1, p2, p3)    
+    triangle = Polygon(p1, p2, p3)
     triangle.setOutline("black")
     triangle.draw(win)
-
 
     message = (
         f"The vertices of the triangle are: "
@@ -46,8 +50,7 @@ def main():
         "Click anywhere to exit."
     )
     message_display.setText(message)
-
-
     win.getMouse()
+
 
 main()
